ffcc6dd9fe7a270a32aafd438f5193cbec30b6f7
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2024-07-19 19:53:41 +0300

    Basic Implementation of the Tile class

--- Server/src/main/java/gr/uop/GameEngine/Tile.java
+++ Server/src/main/java/gr/uop/GameEngine/Tile.java
@@ -4,4 +4,46 @@ public class Tile {
 
     private Integer upperValue;
     private Integer bottomValue;
+
+    public Tile(Integer upperValue, Integer bottomValue) {
+        this.upperValue = upperValue;
+        this.bottomValue = bottomValue;
+    }
+
+    public Integer getUpperValue() {
+        return this.upperValue;
+    }
+
+    public Integer getBottomValue() {
+        return this.bottomValue;
+    }
+
+    public Boolean fits(Tile otherTile) {
+
+        if (this.upperValue == otherTile.upperValue || this.upperValue == otherTile.bottomValue ||
+        this.bottomValue == otherTile.upperValue || this.bottomValue == otherTile.bottomValue) {
+
+            return true;
+
+        }
+        return false;
+    }
+
+    public Boolean fits(int value) {
+        if (this.upperValue == value || this.bottomValue == value) {
+            return true;
+        }
+        return false;
+    }
+
+    public void invert() {
+        int temp = this.upperValue;
+        this.upperValue = this.bottomValue;
+        this.bottomValue = temp;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + upperValue + ":" + bottomValue + "]";
+    }
 }
