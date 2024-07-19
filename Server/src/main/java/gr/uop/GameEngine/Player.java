13a57a0c7b066f63ab47e620b3e48b4e33e76b93
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2024-07-19 18:37:18 +0300

    Basic Player class implementation

--- Server/src/main/java/gr/uop/GameEngine/Player.java
+++ Server/src/main/java/gr/uop/GameEngine/Player.java
@@ -3,7 +3,45 @@ package gr.uop.GameEngine;
 import java.util.ArrayList;
 
 public class Player {
-    
+
     private String name;
-    private ArrayList<Integer> tiles = new ArrayList<>();
+    private ArrayList<Tile> tiles = new ArrayList<>();
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Tile> getTiles() {
+        return this.tiles;
+    }
+
+    public void addTile(Tile tile) {
+        tiles.add(tile);
+    }
+
+    public void removeTile(Tile tile) {
+        tiles.remove(tile);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void updateScore(int points) {
+        this.score += points;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", tiles=" + tiles +
+                ", score=" + score +
+                '}';
+    }
 }
