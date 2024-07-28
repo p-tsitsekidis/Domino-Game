3a5fa1af0299a12627ceadb914480396d30b10ae
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2024-07-28 16:43:10 +0300

    Initialize stock, player tiles and player instances

--- Server/src/main/java/gr/uop/GameEngine/GameEngine.java
+++ Server/src/main/java/gr/uop/GameEngine/GameEngine.java
@@ -1,5 +1,53 @@
 package gr.uop.GameEngine;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Random;
+
 public class GameEngine {
     
+    private List<Tile> stock;
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private List<Tile> lineOfPlay;
+
+    public GameEngine(String player1Name, String player2Name) {
+        stock = createDominoStock();
+        Collections.shuffle(stock);
+
+        List<Tile> player1Tiles = new ArrayList<>();
+        List<Tile> player2Tiles = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            player1Tiles.add(stock.remove(stock.size() - 1));
+            player2Tiles.add(stock.remove(stock.size() - 1));
+        }
+
+        player1 = new Player(player1Name, player1Tiles);
+        player2 = new Player(player2Name, player2Tiles);
+        currentPlayer = player1;
+
+        lineOfPlay = new ArrayList<>();
+    }
+
+    private List<Tile> createDominoStock() {
+        List<Tile> tiles = new ArrayList<>();
+        for (int i = 0; i <= 6; i++) {
+            for (int j = 0; j <= i; j++) {
+                tiles.add(new Tile(i, j));
+            }
+        }
+        return tiles;
+    }
+
+    // public List getOpenEnds() {
+
+    // }
+
+    // public LinkedHashMap getLineOfPlay() {
+        
+    // }
 }
