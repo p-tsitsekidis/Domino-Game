c488b12723d87ba13fdc6000b6afa93c55c8813e
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2024-07-28 21:18:56 +0300

    Made stock use a deque data structure

--- Server/src/main/java/gr/uop/GameEngine/GameEngine.java
+++ Server/src/main/java/gr/uop/GameEngine/GameEngine.java
@@ -2,13 +2,15 @@ package gr.uop.GameEngine;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class GameEngine {
     
-    private List<Tile> stock;
+    private Deque<Tile> stock;
     private Player player1;
     private Player player2;
     private Player currentPlayer;
@@ -16,14 +18,14 @@ public class GameEngine {
 
     public GameEngine(String player1Name, String player2Name) {
         stock = createDominoStock();
-        Collections.shuffle(stock);
+        Collections.shuffle((List<?>) stock);
 
         List<Tile> player1Tiles = new ArrayList<>();
         List<Tile> player2Tiles = new ArrayList<>();
 
         for (int i = 0; i < 7; i++) {
-            player1Tiles.add(stock.remove(stock.size() - 1));
-            player2Tiles.add(stock.remove(stock.size() - 1));
+            player1Tiles.add(stock.pop());
+            player2Tiles.add(stock.pop());
         }
 
         player1 = new Player(player1Name, player1Tiles);
@@ -33,8 +35,8 @@ public class GameEngine {
         lineOfPlay = new ArrayList<>();
     }
 
-    private List<Tile> createDominoStock() {
-        List<Tile> tiles = new ArrayList<>();
+    private Deque<Tile> createDominoStock() {
+        Deque<Tile> tiles = new LinkedList<>();
         for (int i = 0; i <= 6; i++) {
             for (int j = 0; j <= i; j++) {
                 tiles.add(new Tile(i, j));
